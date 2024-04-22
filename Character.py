113e6385eeff5b69bb8fa2f6d8efe1bd8526f82b
Oleg2265 <oleg.stryan@gmail.com>
2024-04-22 16:28:56 +0300

    lesson1

---
+++ Character.py
@@ -0,0 +1,77 @@
+import random
+
+import pygame
+
+bullets = []
+class Player:
+    def __init__(self, x, y, w, h, img, speed):
+        self.photo = pygame.transform.scale(pygame.image.load(img), (w, h))
+        self.hitbox = self.photo.get_rect()
+        self.hitbox.x = x
+        self.hitbox.y = y
+        self.speed = speed
+        self.bullets = []
+
+
+
+    def draw(self, window):
+        #pygame.draw.rect(window, (0,0,255), self.hitbox)
+        window.blit(self.photo, (self.hitbox.x, self.hitbox.y))
+        for bullet in self.bullets:
+            bullet.draw(window)
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.hitbox.y -= self.speed
+        if keys[pygame.K_s]:
+            self.hitbox.y += self.speed
+        if keys[pygame.K_a]:
+            self.hitbox.x -= self.speed
+        if keys[pygame.K_d]:
+            self.hitbox.x += self.speed
+        if keys[pygame.K_SPACE]:
+            self.bullets.append(Bullet(self.hitbox.x, self.hitbox.y,15,30,"bullet.png",2))
+        for bullet in self.bullets:
+            bullet.move()
+    def patron(self):
+        bullets.append(Bullet(self.photo, (self.hitbox.x, self.hitbox.y)))
+
+
+class Bullet:
+    def __init__(self, x, y, w, h, img, speed):
+        self.photo = pygame.transform.scale(pygame.image.load(img), (w, h))
+        self.hitbox = self.photo.get_rect()
+        self.hitbox.x = x
+        self.hitbox.y = y
+        self.speed = speed
+
+
+
+    def draw(self, window):
+        #pygame.draw.rect(window, (0,0,255), self.hitbox)
+        window.blit(self.photo, (self.hitbox.x, self.hitbox.y))
+
+    def move(self):
+
+        self.hitbox.y -= self.speed
+
+class Enemy:
+    def __init__(self, x, y, w, h, img, speed):
+        self.photo = pygame.transform.scale(pygame.image.load(img), (w, h))
+        self.hitbox = self.photo.get_rect()
+        self.hitbox.x = x
+        self.hitbox.y = y
+        self.speed = speed
+
+
+
+    def draw(self, window):
+        #pygame.draw.rect(window, (0,0,255), self.hitbox)
+        window.blit(self.photo, (self.hitbox.x, self.hitbox.y))
+
+    def move(self):
+        self.hitbox.y += self.speed
+        if self.hitbox.y > 500:
+            self.hitbox.x = random.randint(0,700)
+            self.hitbox.y = random.randint(0,500)
